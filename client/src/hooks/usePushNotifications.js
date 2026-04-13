ba499d2ffaaa9a002e3a15f771a78be18e641fc5
boutarfa ahmed <ahmed.boutarfa.2003@gmail.com>
2026-04-13 01:13:17 +0100

    Add SW registration logging

--- client/src/hooks/usePushNotifications.js
+++ client/src/hooks/usePushNotifications.js
@@ -3,9 +3,13 @@ import { useEffect } from "react";
 export function usePushNotifications() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sw.js").catch((err) => {
-        console.error("Service worker registration failed:", err);
-      });
+      navigator.serviceWorker.register("/sw.js")
+        .then((reg) => console.log("Service worker registered:", reg.scope))
+        .catch((err) => {
+          console.error("Service worker registration failed:", err);
+        });
+    } else {
+      console.log("Service workers not supported");
     }
   }, []);
 }
